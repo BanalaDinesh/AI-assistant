a57937117ff5383ea8da69595cf9a682c277cd09
BanalaDinesh <dineshbanala101@gmail.com>
2026-07-06 13:27:19 +0530

    Set up React frontend with Vite and create first component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div>
+      <h1>AI Customer Support Ticket Assistant</h1>
+
+      <p>Developed by Dinesh</p>
+
+
+
+      <p>
+       This application helps users create support tickets and interact with an AI assistant.
+      </p>
+    </div>
+  );
+}
+
+export default App;
